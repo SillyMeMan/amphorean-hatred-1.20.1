c63ab2f5f2e94945d7912b42408f7322b9da1d11
Vinh Gaming <huuvinhn053@gmail.com>
2026-05-14 12:49:59 +0700

    Changed the mixin injection point for screenshakes

---
+++ src/main/java/net/vinh/hatred/mixin/accessor/CameraAccessor.java
@@ -0,0 +1,11 @@
+package net.vinh.hatred.mixin.accessor;
+
+import net.minecraft.client.render.Camera;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Camera.class)
+public interface CameraAccessor {
+    @Invoker("setRotation")
+    void hatred$invokeSetRotation(float yaw, float pitch);
+}
--- src/main/java/net/vinh/hatred/mixin/client/CameraMixin.java
+++ src/main/java/net/vinh/hatred/mixin/client/CameraMixin.java
@@ -4,30 +4,42 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Camera;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.noise.PerlinNoiseSampler;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockView;
 import net.vinh.hatred.client.camera.ScreenshakeController;
+import net.vinh.hatred.mixin.accessor.CameraAccessor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Camera.class)
 public class CameraMixin {
+    private static final PerlinNoiseSampler sampler = new PerlinNoiseSampler(Random.createLocal());
+
     @Shadow private float yaw;
     @Shadow private float pitch;
 
-    @Inject(method = "update", at = @At("TAIL"))
+    @Inject(method = "update", at = @At("RETURN"))
     private void hatred$shake(BlockView area, Entity focusedEntity, boolean thirdPerson, boolean inverseView, float tickDelta, CallbackInfo ci) {
+        Camera self = (Camera)(Object) this;
         float strength = ScreenshakeController.getShakeStrength();
 
         if (strength <= 0f) return;
 
-        MinecraftClient client = MinecraftClient.getInstance();
+        float yawOffset = randomizeOffset(strength, 10);
+        float pitchOffset = randomizeOffset(strength, -10);
+        ((CameraAccessor) self).hatred$invokeSetRotation(self.getYaw() + yawOffset, self.getPitch() + pitchOffset);
+    }
 
-        float time = (client.world.getTime() + tickDelta) * 0.6f;
-
-        this.yaw += MathHelper.sin(time * 3.0f) * strength;
-        this.pitch += MathHelper.cos(time * 2.5f) * strength * 0.7f;
+    @Unique
+    private static float randomizeOffset(float strength, int offset) {
+        float min = -strength * 2;
+        float max = strength * 2;
+        float sampled = (float) sampler.sample((MinecraftClient.getInstance().world.getTime() % 24000L + MinecraftClient.getInstance().getTickDelta())/strength, offset, 0) * 1.5f;
+        return min >= max ? min : sampled * max;
     }
 }
