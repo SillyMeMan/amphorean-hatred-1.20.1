9d27bbdd5589ee1fb7b007e9b9676580716a5af1
Vinh Gaming <huuvinhn053@gmail.com>
2026-07-11 10:31:47 +0700

    exposed the DamageApi

---
+++ src/main/java/net/vinh/hatred/api/damage/DamageApi.java
@@ -0,0 +1,75 @@
+package net.vinh.hatred.api.damage;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.DamageTypeTags;
+import net.minecraft.util.math.Vec3d;
+import net.vinh.hatred.mixin.accessor.LivingEntityAccessor;
+
+/**
+ * The main implementation of the damage system. Injected into {@link Entity} via Loom. Any mixins should target the methods in this class.
+ */
+public class DamageApi {
+    public static boolean damage(Entity target, float totalDamage, DamageDistributor distributor, DamageContext ctx) {
+        RegistryEntry<DamageType> finalType = ctx.type() != null ? ctx.type() : target.getDamageSources().generic().getTypeRegistryEntry();
+
+        ContextAwareDamageSource source = new ContextAwareDamageSource(finalType, ctx);
+
+        if(ctx.trueDamage() && target instanceof LivingEntity living) return applyTrueDamage(living, distributor.distribute(totalDamage), ctx, source);
+
+        boolean damageApplied = target.damage(source, distributor.distribute(totalDamage));
+
+        if(damageApplied) {
+            if(ctx.hitEffects() != null && target instanceof LivingEntity living) ctx.hitEffects().forEach(statusEffectInstance -> living.addStatusEffect(statusEffectInstance, ctx.attacker()));
+
+            if(ctx.knockback() != null) {
+                target.setVelocity(ctx.knockback());
+            } else {
+                target.setVelocity(Vec3d.ZERO);
+            }
+
+            target.velocityModified = true;
+        }
+
+        return damageApplied;
+    }
+
+    private static boolean applyTrueDamage(LivingEntity target, float damage, DamageContext ctx, ContextAwareDamageSource source) {
+        if (target.isInvulnerableTo(source) || (target instanceof PlayerEntity player && player.getAbilities().invulnerable && !source.isIn(DamageTypeTags.BYPASSES_INVULNERABILITY))) {
+            return false;
+        } else if (target.getWorld().isClient) {
+            return false;
+        } else if (target.isDead()) {
+            return false;
+        } else if (source.isIn(DamageTypeTags.IS_FIRE) && target.hasStatusEffect(StatusEffects.FIRE_RESISTANCE)) {
+            return false;
+        } else {
+            float health = target.getHealth();
+            float newHealth = ctx.nonFatal() ? Math.max(1f, health - damage) : Math.max(0f, health - damage);
+            RegistryEntry<DamageType> finalType = ctx.type() != null ? ctx.type() : target.getDamageSources().generic().getTypeRegistryEntry();
+
+            target.getDamageTracker().onDamage(
+                    new ContextAwareDamageSource(finalType, ctx),
+                    damage
+            );
+
+            target.setHealth(newHealth);
+
+            ((LivingEntityAccessor) target).hatred$invokePlayHurtSound(new ContextAwareDamageSource(finalType, ctx));
+
+            target.timeUntilRegen = 20;
+            target.hurtTime = 10;
+            target.maxHurtTime = 10;
+
+            if (newHealth <= 0) {
+                target.onDeath(new ContextAwareDamageSource(finalType, ctx));
+            }
+
+            return true;
+        }
+    }
+}
--- src/main/java/net/vinh/hatred/internal/entity/EntityInjectionAccess.java
+++ src/main/java/net/vinh/hatred/internal/entity/EntityInjectionAccess.java
@@ -8,10 +8,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.registry.tag.DamageTypeTags;
 import net.minecraft.util.math.Vec3d;
-import net.vinh.hatred.api.damage.ContextAwareDamageSource;
-import net.vinh.hatred.api.damage.DamageContext;
-import net.vinh.hatred.api.damage.DamageDistributor;
-import net.vinh.hatred.api.damage.DamageDistributors;
+import net.vinh.hatred.api.damage.*;
 import net.vinh.hatred.internal.data.accessor.EntityMixinAccessor;
 import net.vinh.hatred.internal.scheduler.EntityScheduler;
 import net.vinh.hatred.internal.scheduler.ScheduledTask;
@@ -30,66 +27,10 @@ public interface EntityInjectionAccess {
 
     default boolean damage(float totalDamage, DamageDistributor distributor, DamageContext ctx) {
         Entity target = (Entity) this;
-
-        RegistryEntry<DamageType> finalType = ctx.type() != null ? ctx.type() : target.getDamageSources().generic().getTypeRegistryEntry();
-
-        ContextAwareDamageSource source = new ContextAwareDamageSource(finalType, ctx);
-
-        if(ctx.trueDamage() && target instanceof LivingEntity living) return applyTrueDamage(living, distributor.distribute(totalDamage), ctx, source);
-
-        boolean damageApplied = target.damage(source, distributor.distribute(totalDamage));
-
-        if(damageApplied) {
-            if(ctx.hitEffects() != null && target instanceof LivingEntity living) ctx.hitEffects().forEach(statusEffectInstance -> living.addStatusEffect(statusEffectInstance, ctx.attacker()));
-
-            if(ctx.knockback() != null) {
-                target.setVelocity(ctx.knockback());
-            } else {
-                target.setVelocity(Vec3d.ZERO);
-            }
-
-            target.velocityModified = true;
-        }
-
-        return damageApplied;
+        return DamageApi.damage(target, totalDamage, distributor, ctx);
     }
 
     default boolean damage(float damage, DamageContext ctx) {
         return damage(damage, DamageDistributors.FULL_DAMAGE, ctx);
-    }
-
-    private boolean applyTrueDamage(LivingEntity target, float damage, DamageContext ctx, ContextAwareDamageSource source) {
-        if (target.isInvulnerableTo(source) || (target instanceof PlayerEntity player && player.getAbilities().invulnerable && !source.isIn(DamageTypeTags.BYPASSES_INVULNERABILITY))) {
-            return false;
-        } else if (target.getWorld().isClient) {
-            return false;
-        } else if (target.isDead()) {
-            return false;
-        } else if (source.isIn(DamageTypeTags.IS_FIRE) && target.hasStatusEffect(StatusEffects.FIRE_RESISTANCE)) {
-            return false;
-        } else {
-            float health = target.getHealth();
-            float newHealth = ctx.nonFatal() ? Math.max(1f, health - damage) : Math.max(0f, health - damage);
-            RegistryEntry<DamageType> finalType = ctx.type() != null ? ctx.type() : target.getDamageSources().generic().getTypeRegistryEntry();
-
-            target.getDamageTracker().onDamage(
-                    new ContextAwareDamageSource(finalType, ctx),
-                    damage
-            );
-
-            target.setHealth(newHealth);
-
-            ((LivingEntityAccessor) target).hatred$invokePlayHurtSound(new ContextAwareDamageSource(finalType, ctx));
-
-            target.timeUntilRegen = 20;
-            target.hurtTime = 10;
-            target.maxHurtTime = 10;
-
-            if (newHealth <= 0) {
-                target.onDeath(new ContextAwareDamageSource(finalType, ctx));
-            }
-
-            return true;
-        }
     }
 }
